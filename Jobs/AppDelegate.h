dbff18b4b875dcf62578e2e51663fef044ff0d46
Rick Royd Aban <Velosi@Velosis-MacBook-Air.local>
2014-09-25 17:36:05 +0800

    Initial Commit

---
+++ Jobs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jobs
+//
+//  Created by Rick Royd Aban on 9/25/14.
+//  Copyright (c) 2014 applusvelosi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jobs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jobs
+//
+//  Created by Rick Royd Aban on 9/25/14.
+//  Copyright (c) 2014 applusvelosi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
